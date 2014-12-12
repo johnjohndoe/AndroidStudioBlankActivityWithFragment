9c298c932e22c278d5138c0612937bf5a441ef85
Tobias Preuss <tobias.preuss@googlemail.com>
2014-12-12 16:40:14 +0100

    Import template from Android Studio 1.0 RC 4.

--- app/src/main/java/com/example/androidstudioblankactivitywithfragment/MainActivity.java
+++ app/src/main/java/com/example/androidstudioblankactivitywithfragment/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+                Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
