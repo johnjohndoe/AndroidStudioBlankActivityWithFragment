cf11be4503b062eeb5bae9762d8ca8bb0ee9e18c
Tobias Preuss <tobias.preuss@googlemail.com>
2015-09-16 15:00:07 +0200

    Import template from Android Studio 1.4 Beta 4.

--- app/src/main/java/com/example/androidstudioblankactivitywithfragment/MainActivityFragment.java
+++ app/src/main/java/com/example/androidstudioblankactivitywithfragment/MainActivityFragment.java
@@ -1,11 +1,10 @@
 package com.example.androidstudioblankactivitywithfragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,7 +16,7 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
